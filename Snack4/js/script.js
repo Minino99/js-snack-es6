6827e39792ee14a82d3be2b7cfce886aa61c597d
Minino99 <loizzo.pasqualino@gmail.com>
2022-06-07 16:03:07 +0200

    commit

--- Snack4/js/script.js
+++ Snack4/js/script.js
@@ -1,42 +1,32 @@
 let students = [
- { name: "Marco", id: 213, grades: 78 },
- { name: "Paola", id: 110, grades: 96 },
- { name: "Andrea", id: 250, grades: 48 },
- { name: "Gaia", id: 145, grades: 74 },
- { name: "Luigi", id: 196, grades: 68 },
- { name: "Piero", id: 102, grades: 50 },
- { name: "Francesca", id: 120, grades: 84 },
+  { name: "Marco", id: 213, grades: 78 },
+  { name: "Paola", id: 110, grades: 96 },
+  { name: "Andrea", id: 250, grades: 48 },
+  { name: "Gaia", id: 145, grades: 74 },
+  { name: "Luigi", id: 196, grades: 68 },
+  { name: "Piero", id: 102, grades: 50 },
+  { name: "Francesca", id: 120, grades: 84 },
 ];
 
 // Esercizio 1
 
 const targhetteArray = [];
 
-students.forEach((name, i) => {
-let capitalName = (students[i].name).toUpperCase();
-targhetteArray.push(capitalName);
+students.forEach((studente, i) => {
+  let capitalName = studente[i].name.toUpperCase();
+  targhetteArray.push(capitalName);
 });
 
-console.table(targhetteArray)
+console.table(targhetteArray);
 
 // Esercizio 2
 
-const goodGrades = students.filter((grades, i) => {
-if (students[i].grades >= 70) {
-return true;
-}
-});
+const goodGrades = students.filter((studente, i) => studente[i].grades >= 70);
 
 console.table(goodGrades);
 
-
 // Esercizio 3
 
-const raccomandati = students.filter((grades, i) => {
- if (students[i].grades >= 70 && students[i].id >= 120) {
- return true;
- }
- });
+const raccomandati = students.filter((studente, i) => studente[i].grades >= 70 && studente[i].id >= 120);
 
- console.table(raccomandati);
- 
+console.table(raccomandati);
